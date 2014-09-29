27f7c70633874361d551087d3e817a8500a148fd
Mikko Haapoja <me@mikkoh.com>
2014-09-29 08:57:08 -0400

    Added in a menu

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ var fs = require( 'fs' );
 var bigwheel = require( 'bigwheel' ),
 	bwmodel = require( 'bw-objmodel' ),
 	bwhandlebars = require( 'bw-handlebars' ),
+	handlebars = require( 'handlebars' ),
 	bwwritedom = require( 'bw-writedom' ),
 	bwboiler = require( 'bw-boilersection' );
 
 var MODEL_DATA = JSON.parse( fs.readFileSync( './data/model.json', 'utf8' ) ),
 	TEMPLATE_BASIC = fs.readFileSync( './data/basicPage.hbs', 'utf8' ),
+	TEMPLATE_MENU = fs.readFileSync( './data/menu.hbs', 'utf8' ),
 	model = bwmodel( MODEL_DATA ),
-	handlebars = bwhandlebars( TEMPLATE_BASIC ),
+	template = bwhandlebars( TEMPLATE_BASIC ),
 	writebody = bwwritedom( 'body' ),
 	section = bwboiler();
 
+handlebars.registerPartial( 'menu', TEMPLATE_MENU );
+
 var framework = bigwheel( {
 
-	'/': model( 'landing' )( handlebars )( writebody )( section )(),
-	'/about': model( 'about' )( handlebars )( writebody )( section )(),
-	'/contact': model( 'contact' )( handlebars )( writebody )( section )()
+	'/': model( 'landing' )( template )( writebody )( section )(),
+	'/about': model( 'about' )( template )( writebody )( section )(),
+	'/contact': model( 'contact' )( template )( writebody )( section )()
 });
+
 
 framework.init();

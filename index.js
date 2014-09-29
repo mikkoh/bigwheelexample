14030d28585c24c4b12a23ae4f58786da86fb5b4
Mikko Haapoja <me@mikkoh.com>
2014-09-29 00:27:22 -0400

    You've been had by the snake

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require( 'fs' );
+
+var bigwheel = require( 'bigwheel' ),
+	bwmodel = require( 'bw-objmodel' ),
+	bwhandlebars = require( 'bw-handlebars' ),
+	bwwritedom = require( 'bw-writedom' ),
+	bwboiler = require( 'bw-boilersection' );
+
+var MODEL_DATA = JSON.parse( fs.readFileSync( './data/model.json', 'utf8' ) ),
+	TEMPLATE_BASIC = fs.readFileSync( './data/basicPage.hbs', 'utf8' ),
+	model = bwmodel( MODEL_DATA ),
+	handlebars = bwhandlebars( TEMPLATE_BASIC ),
+	writebody = bwwritedom( 'body' )
+	section = bwboiler();
+
+var framework = bigwheel( {
+
+	'/': model( 'landing' )( handlebars )( writebody )( section )(),
+	'/about': model( 'about' )( handlebars )( writebody )( section )(),
+	'/contact': model( 'contact' )( handlebars )( writebody )( section )()
+});
+
+framework.init();
